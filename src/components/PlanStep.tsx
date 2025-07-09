622a072ee0db893daedff47fc1645930fbe9c48d
ofumi529 <ohsuga.fumiaki@takenaka.co.jp>
2025-07-09 21:19:15 +0900

    fix: make 'reselect works' button fixed at bottom for mobile on plan step

--- src/components/PlanStep.tsx
+++ src/components/PlanStep.tsx
@@ -14,14 +14,14 @@ export default function PlanStep({ origin, originCoords, onRestart }: Props) {
     const [routeCoords, setRouteCoords] = useState<[number, number][]>([]);
 
   return (
-    <div className="h-full flex flex-col">
+    <div className="h-full flex flex-col relative">
       <div className="flex-1 grid grid-rows-[300px_1fr] min-h-0">
         {/* Map showing generated route */}
         <div className="border-b relative">
           <MapView route={routeCoords} />
         </div>
         {/* Travel plan stats */}
-        <div className="overflow-y-auto p-4 bg-gray-50 min-h-0">
+        <div className="overflow-y-auto p-4 bg-gray-50 min-h-0 pb-20">
           <TravelPlanPanel
             onRouteReady={setRouteCoords}
             origin={origin}
@@ -29,8 +29,10 @@ export default function PlanStep({ origin, originCoords, onRestart }: Props) {
           />
         </div>
       </div>
-      <div className="border-t p-4 bg-stone-800 text-center">
-        <Button variant="secondary" onClick={onRestart} className="text-white">
+      
+      {/* Fixed button at bottom */}
+      <div className="fixed bottom-0 left-0 right-0 p-4 bg-stone-800 border-t shadow-lg z-50">
+        <Button variant="secondary" onClick={onRestart} className="text-white w-full">
           作品を選び直す
         </Button>
       </div>
